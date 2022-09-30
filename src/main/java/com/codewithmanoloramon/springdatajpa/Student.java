b689bb0c6f9d240c1fc1c1f5757e732ae7508c19
Manolo-Ramon <manoloramonmanatlao@gmail.com>
2022-09-30 12:45:45 +0400

    Create Student Model

---
+++ src/main/java/com/codewithmanoloramon/springdatajpa/Student.java
@@ -0,0 +1,71 @@
+package com.codewithmanoloramon.springdatajpa;
+
+public class Student {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String age;
+
+    public Student() {
+    }
+
+    public Student(Long id, String firstName, String lastName, String email, String age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
