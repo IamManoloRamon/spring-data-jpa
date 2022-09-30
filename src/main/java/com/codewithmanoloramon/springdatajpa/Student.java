f0008ac47ab9d51eca81aeaa8a42cbcbbd4c305b
Manolo-Ramon <manoloramonmanatlao@gmail.com>
2022-09-30 13:02:11 +0400

    Annotate for data mapping for creating student table.

--- src/main/java/com/codewithmanoloramon/springdatajpa/Student.java
+++ src/main/java/com/codewithmanoloramon/springdatajpa/Student.java
@@ -1,10 +1,59 @@
 package com.codewithmanoloramon.springdatajpa;
 
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "Student")
+@Table(
+        name = "student",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "student_email_unique", columnNames = "email")
+        }
+)
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "student_sequence"
+    )
+
+    @Column(
+            name = "id",
+            nullable = false
+    )
     private Long id;
+
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String firstName;
+
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String lastName;
+
+    @Column(
+            name = "email",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String email;
+
+    @Column(
+            name = "age",
+            nullable = false
+    )
     private String age;
 
     public Student() {
